c4df6e66a2d5438469bb8cc7b18fdc659f13496f
Petr Michal <122700272+petrmichal0@users.noreply.github.com>
2024-10-15 15:16:14 +0200

    Update expenseRoutes.js

--- routes/expenseRoutes.js
+++ routes/expenseRoutes.js
@@ -1,22 +1,16 @@
 const express = require("express");
 const expenseController = require("../controllers/expenseController");
 
-// Initialize the Express router
 const router = express.Router();
 
-// Define routes for expenses
-// GET: Fetch all expenses, POST: Create a new expense
 router
   .route("/")
   .get(expenseController.getAllExpenses)
   .post(expenseController.createExpense);
 
-// Define routes for specific expense by ID
-// PATCH: Update an existing expense by ID, DELETE: Remove an expense by ID
 router
   .route("/:id")
   .patch(expenseController.updateExpense)
   .delete(expenseController.deleteExpense);
 
-// Export the router to be used in the main app
 module.exports = router;
