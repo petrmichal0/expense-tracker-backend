8a41d728586c3e9627c7edbb5e4564be1f6b4c5e
Petr Michal <122700272+petrmichal0@users.noreply.github.com>
2024-10-15 14:51:34 +0200

    Update expenseModel.js

--- models/expenseModel.js
+++ models/expenseModel.js
@@ -1,20 +1,25 @@
 const mongoose = require("mongoose");
 
+// Define the schema for expenses
+// This outlines the structure of the expense document in the MongoDB database
 const expenseSchema = new mongoose.Schema({
   amount: {
-    type: Number,
-    required: true,
+    type: Number, // The amount spent
+    required: true, // This field is required
   },
   date: {
-    type: Date,
-    required: true,
+    type: Date, // The date of the expense
+    required: true, // This field is required
   },
   description: {
-    type: String,
-    required: true,
+    type: String, // A description of the expense
+    required: true, // This field is required
   },
 });
 
+// Create the Expense model based on the schema
+// This model represents the expense collection in the database
 const Expense = mongoose.model("Expense", expenseSchema);
 
+// Export the Expense model so it can be used elsewhere in the application
 module.exports = Expense;
