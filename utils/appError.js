8e71b9867c11c862727e6fe7130342688828c6d6
Petr Michal <122700272+petrmichal0@users.noreply.github.com>
2024-10-15 13:08:54 +0200

    Update appError.js

--- utils/appError.js
+++ utils/appError.js
@@ -1,12 +1,10 @@
-class AppError extends Error {
-  constructor(message, statusCode) {
-    super(message);
-    this.statusCode = statusCode;
-    this.status = `${statusCode}`.startsWith("4") ? "fail" : "error";
-    this.isOperational = true;
-
-    Error.captureStackTrace(this, this.constructor);
-  }
+function AppError(message, statusCode) {
+  const error = new Error(message); // We make a new Error object with a message
+  error.statusCode = statusCode; // We add a statusCode to the error
+  error.status = `${statusCode}`.startsWith("4") ? "fail" : "error"; // If statusCode starts with 4, it's a "fail"; if not, it's an "error"
+  error.isOperational = true; // This is an operational error, not a technical one
+  Error.captureStackTrace(error, AppError); // We capture where the error happened
+  return error; 
 }
 
-module.exports = AppError;
+module.exports = AppError; 
